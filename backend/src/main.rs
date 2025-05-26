8e72139de1fbd0be8841c0ed431eab82365f18c1
tobinio <Tobias.frischmann1@gmail.com>
2025-05-26 12:00:52 +0200

    set port and project_dir via env

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{env, sync::Arc};
 
 use backend::{
     app,
@@ -8,18 +8,26 @@ use tracing::info;
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+
+    let port = env::var("PORT").unwrap_or_else(|_| "8081".to_string());
+    let project_dir = env::var("PROJECT_DIR").unwrap_or_else(|_| "./tests/projects/".to_string());
+
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8081").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
 
     info!("listening on {}", listener.local_addr().unwrap());
+    info!("using project path '{}'", project_dir.clone());
 
     axum::serve(
         listener,
         app(
-            Arc::new(ProjectService::new("./tests/projects/".into())),
+            Arc::new(ProjectService::new(project_dir.into())),
             Arc::new(ContainerService),
         ),
     )
