d7579b82b8ed688d3184e76a8ecd965754eb1f2d
tobinio <Tobias.frischmann1@gmail.com>
2025-05-29 12:36:24 +0200

    use nuxtLink

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { NuxtLink } from "#components";
+
 const config = useRuntimeConfig();
 const { data } = useFetch<Project[]>("/projects", {
   baseURL: config.public.apiURL,
@@ -10,7 +12,7 @@ const { data } = useFetch<Project[]>("/projects", {
     <h2 class="text-4xl pb-4">Projects</h2>
     <div class="flex gap-2 flex-row">
       <div v-for="project in data" :key="project.name">
-        <a
+        <NuxtLink
           :href="project.name"
           class="flex gap-2 items-center border-1 border-neutral-600 bg-neutral-700 hover:bg-neutral-600 text-xl shadow-xl px-1"
         >
@@ -20,7 +22,7 @@ const { data } = useFetch<Project[]>("/projects", {
             class="inline-block w-4 aspect-square bg-emerald-700 rounded-full"
             :class="{ 'bg-neutral-800!': project.status == 'stopped' }"
           />
-        </a>
+        </NuxtLink>
       </div>
     </div>
   </div>
